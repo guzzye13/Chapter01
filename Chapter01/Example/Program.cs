7908379466b5e1da78b7d95020239b1181ae7d4e
guzzye13 <101203721+guzzye13@users.noreply.github.com>
2022-10-13 12:54:04 -0400

    Add files via upload

---
+++ Chapter01/Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Temperature on {0:D} is {1}°C.", DateTime.Today, 23.4);
+        }
+    }
+}
